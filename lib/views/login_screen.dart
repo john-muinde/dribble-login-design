c2811d5af0d8264d38ff0d891717315f8318e69f
atrocious-jpg <jhnmuinde@gmail.com>
2023-02-03 21:55:33 +0300

    finished with the login design

--- lib/views/login_screen.dart
+++ lib/views/login_screen.dart
@@ -16,100 +16,145 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Second App'),
+        title: const Text(
+          'Second App',
+          style: TextStyle(
+            fontFamily: 'Ubuntu',
+            fontWeight: FontWeight.bold
+          ),
+        ),
         backgroundColor: colors['secondary2'],
+        centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Stack(
-            alignment: AlignmentDirectional.center,
-            children: [
-              Icon(Icons.shield_outlined,
-                  size: 130, color: colors['secondary']),
-              Icon(
-                Icons.lock_outline_sharp,
-                size: 40,
-                color: colors['secondary3'],
-              ),
-            ],
-          ),
-          Column(
-            children: const [
-              Text(
-                'Welcome to Saifty!',
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              Text('Keep your data safe!',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w200)),
-            ],
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.only(top: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Stack(
+              alignment: AlignmentDirectional.center,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      color: Colors.black12),
-                  child: textFieldWidget(
-                      label: 'Username', controller: _usernameController),
+                Icon(Icons.shield_outlined,
+                    size: 130, color: colors['secondary']),
+                Icon(
+                  Icons.lock_outline_sharp,
+                  size: 40,
+                  color: colors['secondary3'],
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(6),
-                      color: Colors.black12),
-                  child: textFieldWidget(
-                      label: 'Password',
-                      controller: _passwordController,
-                      password: true),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  width: double.infinity,
-                  height: 60,
-                  margin: const EdgeInsets.only(top: 1),
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(
-                        const Color.fromARGB(255, 151, 0, 255),
-                      ),
-                      // 9700ff
-                    ),
-                    onPressed: () {},
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(top: 5),
-                  child: const Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        textBaseline: TextBaseline.alphabetic,
-                        ),
-                  ),
-                )
               ],
             ),
-          ),
-          const Text('Dont have an account?Register',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              textBaseline: TextBaseline.alphabetic,
-              fontFamily: 'Ubuntu'
+            SizedBox(
+              height: 30,
             ),
-          )
-        ],
+            Column(
+              children: const [
+                Text(
+                  'Welcome to Saifty!',
+                  style: TextStyle(
+                    fontFamily: 'Ubuntu',
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'Keep your data safe!',
+                  style: TextStyle(
+                    fontFamily: 'Ubuntu',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w200,
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 40,
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        color: Colors.black12),
+                    child: textFieldWidget(
+                        label: 'Username', controller: _usernameController),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(6),
+                        color: Colors.black12),
+                    child: textFieldWidget(
+                        label: 'Password',
+                        controller: _passwordController,
+                        password: true),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 60,
+                    margin: const EdgeInsets.only(top: 1),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(
+                          const Color.fromARGB(255, 151, 0, 255),
+                        ),
+                        // 9700ff
+                      ),
+                      onPressed: () {},
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(fontFamily: 'Ubuntu', fontSize: 20),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(top: 15),
+                    child: Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        fontFamily: 'Ubuntu',
+                        fontWeight: FontWeight.bold,
+                        textBaseline: TextBaseline.alphabetic,
+                        color: colors['secondary'],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 120,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Dont have an account? ',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      textBaseline: TextBaseline.alphabetic,
+                      fontFamily: 'Ubuntu'),
+                ),
+                Text(
+                  'Register',
+                  style: TextStyle(
+                    fontFamily: 'Ubuntu',
+                    fontWeight: FontWeight.bold,
+                    textBaseline: TextBaseline.alphabetic,
+                    color: colors['secondary'],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
@@ -126,6 +171,7 @@ class LoginScreen extends StatelessWidget {
       obscureText: password,
       obscuringCharacter: '*',
       style: const TextStyle(
+        fontFamily: 'Ubuntu',
         fontWeight: FontWeight.w600,
         fontSize: 20,
       ),
@@ -135,7 +181,8 @@ class LoginScreen extends StatelessWidget {
           border: InputBorder.none,
           floatingLabelAlignment: FloatingLabelAlignment.start,
           suffixIcon: suffixIcon,
-          labelStyle: const TextStyle(color: Colors.grey)),
+          labelStyle:
+              const TextStyle(fontFamily: 'Ubuntu', color: Colors.grey)),
     );
   }
 }
